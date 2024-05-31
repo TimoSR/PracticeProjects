460c0ef1f997d6f527f09f4af5b30a4d20d1f89e
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2024-05-31 22:59:40 +0200

    Done

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,13 +8,13 @@ PrintLib newPrintLib = new PrintLib(newLogger1);
 
 newPrintLib.HelloWorld();
 
-Console.WriteLine(ClassLogger.AMOUNT_OF_LOGGERS);
+Console.WriteLine($"Created Amount {ClassLogger.AMOUNT_OF_LOGGERS} \n");
 
 string @string1 = "StaticClass World!";
 
 StaticClassLogger.Log(@string1);
 
-Console.WriteLine(StaticClassLogger.AMOUNT_OF_LOGGERS);
+Console.WriteLine($"Created Amount {StaticClassLogger.AMOUNT_OF_LOGGERS}\n");
 
 string @string2 = "StaticMethod World 1!";  
 
@@ -24,6 +24,8 @@ string @string3 = "StaticMethod World 2!";
 
 StaticMethods.StaticLog(@string3);
 
-Console.WriteLine(StaticMethods.AMOUNT_OF_LOGGERS);
+Console.WriteLine($"Created Amount {StaticMethods.AMOUNT_OF_LOGGERS}\n");
 
-Console.WriteLine("Static variables and methods belong to the class itself instead of the object.");
+Console.WriteLine("Static variables and methods belong to the class itself instead of the object, therefore can be called on the type.\n");
+
+Console.WriteLine("This is something we see a lot with the types implemented in .NET \n");
