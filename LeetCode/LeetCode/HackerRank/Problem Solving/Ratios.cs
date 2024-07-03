0842dbc35cb21480fec1a749cd3d604c0f6da9e7
Archit3ct <timothy.s.rasmussen@gmail.com>
2024-07-03 20:02:01 +0200

    ArraySum

---
+++ LeetCode/LeetCode/HackerRank/Problem Solving/Ratios.cs
@@ -0,0 +1,33 @@
+namespace LeetCode.Problem_Solving;
+
+public class Ratios
+{
+    public static void plusMinus(List<int> arr)
+    {
+        var count = arr.Count;
+        var positive = 0;
+        var negative = 0;
+        var zeros = 0;
+
+        foreach (var number in arr)
+        {
+            if(number > 0) {
+                positive++;
+            }
+            else if (number < 0) {
+                negative++;
+            }
+            else {
+                zeros++;
+            }
+        }
+        
+        var positiveRatio = Math.Round((decimal) positive / count, 6);
+        var negativeRatio = Math.Round((decimal) negative / count, 6);
+        var zeroRatio = Math.Round((decimal) zeros / count, 6); 
+        
+        Console.WriteLine($"{positiveRatio:F6}");
+        Console.WriteLine($"{negativeRatio:F6}");
+        Console.WriteLine($"{zeroRatio:F6}");
+    }
+}
