a15bd1b69ad159da641ebca95c1a5481d1051cb3
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2024-07-04 09:28:02 +0200

    Update

--- LeetCode/LeetCode/SystemBasics/SpreadElement.cs
+++ LeetCode/LeetCode/SystemBasics/SpreadElement.cs
@@ -4,13 +4,13 @@ namespace LeetCode.SystemBasics;
 
 public class SpreadElement
 {
-    private static readonly int[] row0 = [1, 2, 3];
-    private static readonly int[] row1 = [4, 5, 6];
-    private static readonly int[] row2 = [7, 8, 9];
-
     // spread element .. in C# and ... in JS
     public static void CombineArrays()
     {
+        int[] row0 = [1, 2, 3];
+        int[] row1 = [4, 5, 6];
+        int[] row2 = [7, 8, 9];
+        
         int[] single = [.. row0,.. row1,.. row2];
 
         foreach (var element in single)
