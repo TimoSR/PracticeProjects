2d1c933c015814efc67a40eb6392e97c339260f9
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2024-07-11 14:50:19 +0200

    update

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -6,3 +6,37 @@ WorkingWithStringTemplates.StringStuffFormat();
 WorkingWithStringTemplates.StringStuffInterpolation();*/
 SpreadElement.CombineArrays();
 LongRawStringLiterals.LongString();
+
+List<int> arr = [];
+
+arr.Count();
+
+var template = "[A], [B]!";
+
+var test = template.Replace("[A]", "Hello").Replace("[B]", "World");
+
+var test2 = "Hello World 2!";
+
+Console.WriteLine(test);
+
+foreach (var value in test2)
+{
+    Console.WriteLine(value);
+}
+
+int[] arr3 = [0, 2, 3, 4, 5, 6];
+
+var arr4 = new int[arr3.Length];
+
+foreach (var value in arr3)
+{
+    Console.Write($"{value} ");
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < arr3.Length; i++)
+{
+    arr4[i] = arr3[i];
+    Console.Write($"{arr4[i]} ");
+}
