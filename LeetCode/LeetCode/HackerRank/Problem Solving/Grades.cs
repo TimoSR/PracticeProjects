7613e78660123c7064319835c3260c5580d0a520
Archit3ct <timothy.s.rasmussen@gmail.com>
2024-07-14 23:28:39 +0200

    Grades Flow Control

---
+++ LeetCode/LeetCode/HackerRank/Problem Solving/Grades.cs
@@ -0,0 +1,49 @@
+namespace LeetCode.HackerRank.Problem_Solving;
+
+public class Grades
+{
+    public static List<int> gradingStudents(List<int> grades)
+    {
+
+        List<int> processedGrades = [];
+        
+        foreach(var grade in grades)
+        {
+            if (grade < 38)
+            {
+                processedGrades.Add(grade);
+            }
+            else
+            {
+                var roundedGrade = RoundToNearest5(grade);
+                var distance = roundedGrade - grade;
+
+                if (distance < 3)
+                {
+                    processedGrades.Add(roundedGrade);
+                }
+                else
+                {
+                    processedGrades.Add(grade);
+                }
+            }
+        }
+
+        return processedGrades;
+    }
+    
+    private static int RoundToNearest5(int grade)
+    { 
+        return (int)Math.Round(grade/ 5.0, MidpointRounding.ToPositiveInfinity) * 5;
+    }
+
+    private static bool Fail(int grade)
+    {
+        return grade < 40;
+    }
+
+    private static bool Pass(int grade)
+    {
+        return grade >= 40;
+    }
+}
