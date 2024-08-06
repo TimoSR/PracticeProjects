c594e038486fc11a60c352180aab7e76932d4564
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2024-08-07 00:04:14 +0200

    b

---
+++ LeetCode/LeetCode/HackerRank/Problem Solving/WorkingWithNumbers/Birthday.cs
@@ -0,0 +1,35 @@
+namespace LeetCode.HackerRank.Problem_Solving.WorkingWithNumbers;
+
+public class Birthday
+{
+    public static int birthday(List<int> s, int d, int m)
+    {
+        var count = 0;
+
+        var sumGoal = d;
+        var pieces = m;
+
+        // This assignment is about knowing how to use multiple pointers to walk through an array
+        // To convert the values to work on the array and respect the boundaries of the array length
+        
+        for (var i = 0; i < s.Count; i++)
+        {
+            var lastPiecePosition = i + pieces - 1;
+
+            var sum = s[i];
+            
+            for (var j = i + 1; j <= lastPiecePosition && j < s.Count; j++)
+            {
+                sum += s[j];   
+            }
+            
+            if (sum == sumGoal)
+            {
+                count++;
+            }
+            
+        }
+
+        return count;
+    }
+}
